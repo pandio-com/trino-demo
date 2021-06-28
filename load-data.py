5f62abf47b6c8c183d409a07128fdd16548a5722
Shyam Vyas <shyam@pandio.com>
2021-06-28 12:47:11 +0530

    add dockerfile

--- load-data.py
+++ load-data.py
@@ -1,21 +1,22 @@
 import psycopg2
 import mysql.connector
 import faker
+import os
 
 data = faker.Faker('en_US')
 
 connm = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="admin",
-    database="dev"
+    host=os.environ.get('MYSQL_HOST'),
+    user=os.environ.get('MYSQL_USER'),
+    password=os.environ.get('MYSQL_PASSWORD'),
+    database=os.environ.get('MYSQL_DB')
 )
-
 connp = psycopg2.connect(
-    host="localhost",
-    database="dev",
-    user="admin",
-    password="admin")
+    host=os.environ.get('POSTGRES_HOST'),
+    database=os.environ.get('POSTGRES_DB'),
+    user=os.environ.get('POSTGRES_USER'),
+    password=os.environ.get('POSTGRES_PASSWORD')
+)
 
 curp = connp.cursor()
 curm = connm.cursor()
@@ -29,7 +30,9 @@ curm.execute("CREATE TABLE customers (id VARCHAR(255), name VARCHAR(255))")
 curp.execute("CREATE TABLE customers (id VARCHAR(255), address VARCHAR(255))")
 
 namedict = []
-for i in range(0, 10):
+num_of_rows = os.environ.get('NUMOFROWS')
+num_of_rows = int(num_of_rows)
+for i in range(0, num_of_rows):
     namedict.append({
         'id': getattr(data, 'uuid4')(),
         'name': getattr(data, 'name')(),
